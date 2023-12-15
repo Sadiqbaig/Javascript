35b146d0b17c611d5fcd88e76d36f923d6653959
Sadiqbaig <99716005+Sadiqbaig@users.noreply.github.com>
2023-12-15 20:51:52 +0000

    config done

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,69 @@
+"use strict";  // treat this as all JS code as newer version
+
+// alert("hello");   // We are using node.js not browser
+
+// Code readablity should be must high
+
+// Primitive Datatype
+
+let name="Sadiq";  // String
+let age= "24";    // Number  
+let isLoggedIn= true;  // Boolean
+let population= "10000000000"; // BigInt
+let country;   // undefined [not assigned]   // typeof null is undefined
+// null   standalone value [empty]      // typeof null is object
+// Symbol    unique
+
+// Non-Primitive Datatype
+
+let obj =new Object;  // Object
+
+// ======================================================================================
+
+// let score = "33abc";
+// console.log(typeof score);                  string
+// let stringInNumber = Number(score)
+// console.log(typeof stringInNumber);          number
+// console.log(stringInNumber);                 NaN
+
+
+// let score = null;
+// console.log(typeof score);                   object
+// let stringInNumber = Number(score)
+// console.log(typeof stringInNumber);          number
+// console.log(stringInNumber);                    0
+
+
+// let score = undefined;
+// console.log(typeof score);                 undefined
+// let stringInNumber = Number(score)          
+// console.log(typeof stringInNumber);          number
+// console.log(stringInNumber);                 NaN
+
+// ====================================================================================
+// Operations:
+
+let val = 3;
+let negVal = -val;
+// console.log(negVal);     -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str='Sadiq';
+let str1='Baig';
+// console.log(str+str1);    SadiqBaig
+
+// console.log(1+'2');    12
+// console.log('1'+2 +2);  122
+// console.log(2+2+'1');    41
+// console.log('1'+'2');   12
+// console.log(+true);        1
+// console.log(+"");       0
+
+
+
